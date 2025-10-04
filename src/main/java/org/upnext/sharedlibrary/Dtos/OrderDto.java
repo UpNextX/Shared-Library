dd4c45fcde6e1fda76e60934264297dd666d9029
Eslam Taha <i777slam73@gmail.com>
2025-10-04 17:08:09 +0300

    Updated OrderDto

--- src/main/java/org/upnext/sharedlibrary/Dtos/OrderDto.java
+++ src/main/java/org/upnext/sharedlibrary/Dtos/OrderDto.java
@@ -1,4 +1,26 @@
 package org.upnext.sharedlibrary.Dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.upnext.sharedlibrary.Enums.OrderStatus;
+import org.upnext.sharedlibrary.Enums.PaymentMethod;
+import org.upnext.sharedlibrary.Enums.PaymentStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDto {
+    Long orderId;
+    Long userId;
+    Long totalCost;
+    PaymentStatus paymentStatus;
+    OrderStatus orderStatus;
+    PaymentMethod paymentMethod;
+    LocalDateTime orderDate;
+    LocalDateTime deliveryDate;
+    List<OrderItemDto> orderItems;
 }
