cdc2f1da69209aefcf36a086ac24c35230dc1e83
Eslam Taha <i777slam73@gmail.com>
2025-10-07 17:29:20 +0300

    DTOS Update

--- src/main/java/org/upnext/sharedlibrary/Dtos/OrderPaymentDto.java
+++ src/main/java/org/upnext/sharedlibrary/Dtos/OrderPaymentDto.java
@@ -1,4 +1,5 @@
 package org.upnext.sharedlibrary.Dtos;
+import jakarta.validation.constraints.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,8 +12,24 @@ import org.upnext.sharedlibrary.Enums.PaymentStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderPaymentDto {
+    @NotNull(message = "Order ID cannot be null")
+    @Positive(message = "Order ID must be positive")
     private Long orderId;
+
+    @NotNull(message = "User ID cannot be null")
+    @Positive(message = "User ID must be positive")
     private Long userId;
+
+    @NotNull(message = "Total cost cannot be null")
+    @PositiveOrZero(message = "Total cost must be zero or positive")
     private Double totalCost;
+
+    @NotNull(message = "Payment status cannot be null")
     private PaymentStatus paymentStatus;
+
+    @NotNull(message = "Payment method cannot be null")
+    private PaymentMethod paymentMethod;
+
+    private String paymentTransactionId;
+
 }
---
+++ src/main/java/org/upnext/sharedlibrary/Dtos/StockUpdateRequest.java
@@ -0,0 +1,15 @@
+package org.upnext.sharedlibrary.Dtos;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockUpdateRequest {
+    @NotNull
+    Integer stock;
+}
